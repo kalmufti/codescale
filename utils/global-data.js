c132f9edccf6071767dbba97eb72bd08feacef93
Khalid Almufti <almufti.khalid@gmail.com>
2022-09-17 21:47:41 -0500

    initial blog settings update

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Khalid Almufti';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Code Scales';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Learn it all, and grow fat from knowledge';
 
   return {
     name,
